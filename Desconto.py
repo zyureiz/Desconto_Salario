74e26b471c9af535fd7f47707b26af67e29db663
Luciano Ferreira <lucianoferreira_rocha@outlook.com>
2023-10-23 20:19:17 -0300

    V0.1

---
+++ Desconto.py
@@ -0,0 +1,24 @@
+def calcular_desconto_salario(salario):
+    if salario <= 600:
+        desconto = salario * 0.05  # 5% de imposto
+    elif salario <= 1200:
+        desconto = salario * 0.10  # 10% de imposto
+    elif salario <= 2000:
+        desconto = salario * 0.15  # 15% de imposto
+    elif salario <= 5000:
+        desconto = salario * 0.20  # 20% de imposto
+    else:
+        desconto = salario * 0.25  # 25% de imposto
+    
+    salario_liquido = salario - desconto
+    return salario_liquido
+
+# Solicitar ao usuário que insira o salário
+salario = float(input("\nDigite o salário(em reais): "))
+
+# Calcular o salário líquido após o desconto de imposto
+salario_liquido = calcular_desconto_salario(salario)
+
+# Exibir o valor líquido
+print("\nO Valor líquido após desconto é de: R$",salario_liquido)
+print("\n")
--- README.md
+++
@@ -1,2 +0,0 @@
-# Desconto_Salario
- Algoritmo em python que desconta um valor "x" de acordo com o valor recebido pelo funcionario.
